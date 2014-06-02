17953777266d89aacba2016c55cb277f5688ff7e
Debbie Mowry <debbie@debbiemowry.com>
2014-06-02 14:20:35 -0400

    Version modification needed to force Moodle to read updated access.php.

--- mod/forum/version.php
+++ mod/forum/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 //$module->version   = 2013110500;       // The current module version (Date: YYYYMMDDXX)
-$module->version   = 2014053000;       // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2014053100;       // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2013110500;       // Requires this Moodle version
 $module->component = 'mod_forum';      // Full name of the plugin (used for diagnostics)
 $module->cron      = 60;
